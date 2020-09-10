95a0a07f548871145df12dad96dddfd54349434a
Parul Bansal <bparul999@gmail.com>
2020-09-10 14:51:41 +0530

    Verify

--- Websigninsignup/app/Email/account.js
+++ Websigninsignup/app/Email/account.js
@@ -1,4 +1,4 @@
-const sendGridApiKey = process.env.SENDGRID_API_KEY || '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';
+const sendGridApiKey = process.env.SENDGRID_API_KEY || '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'
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(sendGridApiKey);
